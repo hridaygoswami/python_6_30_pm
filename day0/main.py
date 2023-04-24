24accc445f6af2ef003f4a88fe3c1e9b002c24f9
hridaygoswami <epikhriday@gmail.com>
2023-04-24 19:04:20 +0530

    added files for day0

---
+++ day0/main.py
@@ -0,0 +1,12 @@
+# print("Hello world")
+# print("Hello world \nHow are you?")
+# print("How are you?")
+
+x = """10
+Hriday"""
+y = 10
+# primitve and non-primitive
+# primitive: int, float, string, boolean
+# print("The value of x is",x, "\nThe value of y is", y)
+# print("The value of x is"+x)
+print(f"The value of x is {x}")
